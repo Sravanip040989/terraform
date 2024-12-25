f3a5ccac9db1b9a0c96e42edb5c29fc9aada3f0f
Thanshv <sravanij.1163@gmail.com>
2024-12-25 22:38:54 +0530

    loops

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "db" {
-    count = 3
+    # count = 3
+    count = length(var.instance_names)
     ami = "ami-09c813fb71547fc4f"
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     instance_type = "t3.micro"
